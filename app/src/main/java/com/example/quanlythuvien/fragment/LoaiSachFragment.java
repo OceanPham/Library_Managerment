27b07b04c4e9378b268ec37892c838aa6effbd71
Ocean <127366007+OceanPham@users.noreply.github.com>
2024-03-30 12:32:47 +0700

    Update LoaiSachFragment.java

--- app/src/main/java/com/example/quanlythuvien/fragment/LoaiSachFragment.java
+++ app/src/main/java/com/example/quanlythuvien/fragment/LoaiSachFragment.java
@@ -18,5 +18,6 @@ public class LoaiSachFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_loai_sach, container, false);
+//Loaisach
     }
 }
